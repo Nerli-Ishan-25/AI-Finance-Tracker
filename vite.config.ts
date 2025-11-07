251a40229a50bc9007d67b344cd33b5f161a1a79
Ishan <nerli.ishan@gmail.com>
2025-11-07 19:15:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/AI-Finance-Tracker/',
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
+//base: "/vite-react-gallery/"
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base: '/AI-Finance-Tracker/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
-
-//base: "/vite-react-gallery/",
+  base: '/AI-Finance-Tracker/', // 👈 Replace with your repo name
+}),
